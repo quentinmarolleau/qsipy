bf58cc5cbee7688aa63aa2e2254fd7def61fbfc3
Quentin Marolleau <quentin.marolleau@institutoptique.fr>
2023-06-27 11:31:31 +0200

    testing: proof of principle
    `test_ev_variance_difference_perfect_qe' is working.
    
    We have separated the testing of the points where the function should
    take the value "0" from the other ones.

--- tests/test_tfs.py
+++ tests/test_tfs.py
@@ -4,38 +4,52 @@ from qsipy import tfs
 
 
 def relative_deviation(array_tested, array_tabulated):
-    return np.where(
-        array_tabulated != 0,
-        np.abs(array_tested - array_tabulated) / array_tabulated,
-        np.abs(array_tested - array_tabulated),
-    )
+    return np.abs(array_tested - array_tabulated) / array_tabulated
 
 
-def test_ev_variance_difference_perfect_qe():
-    N_equal_2 = np.array(
+def test_ev_variance_difference_perfect_qe(RELATIVE_DIFF=1e-14):
+    # check value at zero
+    assert tfs.ev_variance_difference_perfect_qe(0, 2) == 0
+    assert tfs.ev_variance_difference_perfect_qe(0, 10) == 0
+    assert tfs.ev_variance_difference_perfect_qe(0, 100) == 0
+
+    # check nonzero values
+    phi_inputs = np.array([0.01, 0.05, 0.1, 0.5, 1.0])
+    tabulated_N_equal_2 = np.array(
         [
-            0.0,
+            0.0000999966667111108,
+            0.00249791736098712,
             0.00996671107937919,
-            0.0394695029985575,
-            0.0873321925451609,
-            0.151646645326417,
             0.22984884706593,
-            0.318821122761663,
-            0.41501642854988,
-            0.514599761150644,
-            0.613601047346544,
             0.708073418273571,
         ]
     )
-    check = (
-        relative_deviation(
-            array_tested=tfs.ev_variance_difference_perfect_qe(
-                np.linspace(0, 1, 11), 2
-            ),
-            array_tabulated=N_equal_2,
-        )
-        < 1e-5
+    tabulated_N_equal_100 = np.array(
+        [
+            0.127495750056666,
+            3.18484463525857,
+            12.7075566262085,
+            293.057280009061,
+            902.793608298803,
+        ]
     )
 
+    check = (
+        relative_deviation(
+            array_tested=tfs.ev_variance_difference_perfect_qe(phi_inputs, 2),
+            array_tabulated=tabulated_N_equal_2,
+        )
+        < RELATIVE_DIFF
+    )
+    check = check.all()
+    assert check
+
+    check = (
+        relative_deviation(
+            array_tested=tfs.ev_variance_difference_perfect_qe(phi_inputs, 100),
+            array_tabulated=tabulated_N_equal_100,
+        )
+        < RELATIVE_DIFF
+    )
     check = check.all()
     assert check
