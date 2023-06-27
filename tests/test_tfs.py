bb0abff9db325d2f0322571cf55ef1a3a1916d91
Quentin Marolleau <quentin.marolleau@institutoptique.fr>
2023-06-27 11:39:00 +0200

    testing: adding 'test_ev_difference_quarted_perfect_qe`

--- tests/test_tfs.py
+++ tests/test_tfs.py
@@ -53,3 +53,51 @@ def test_ev_variance_difference_perfect_qe(RELATIVE_DIFF=1e-14):
     )
     check = check.all()
     assert check
+
+
+def test_ev_difference_quarted_perfect_qe(RELATIVE_DIFF=1e-14):
+    # check value at zero
+    assert tfs.ev_difference_quarted_perfect_qe(0, 2) == 0
+    assert tfs.ev_difference_quarted_perfect_qe(0, 10) == 0
+    assert tfs.ev_difference_quarted_perfect_qe(0, 100) == 0
+
+    # check nonzero values
+    phi_inputs = np.array([0.01, 0.05, 0.1, 0.5, 1.0])
+    tabulated_N_equal_2 = np.array(
+        [
+            0.0000999966667111108,
+            0.00249791736098712,
+            0.00996671107937919,
+            0.22984884706593,
+            0.708073418273571,
+        ]
+    )
+    tabulated_N_equal_100 = np.array(
+        [
+            0.151859375755396,
+            18.3877644433019,
+            254.740570920523,
+            129015.873012571,
+            1222498.37615172,
+        ]
+    )
+
+    check = (
+        relative_deviation(
+            array_tested=tfs.ev_difference_quarted_perfect_qe(phi_inputs, 2),
+            array_tabulated=tabulated_N_equal_2,
+        )
+        < RELATIVE_DIFF
+    )
+    check = check.all()
+    assert check
+
+    check = (
+        relative_deviation(
+            array_tested=tfs.ev_difference_quarted_perfect_qe(phi_inputs, 100),
+            array_tabulated=tabulated_N_equal_100,
+        )
+        < RELATIVE_DIFF
+    )
+    check = check.all()
+    assert check
